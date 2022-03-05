72093d0614ce8705f63ebea837daa0a7fb4aa6a8
Vaxuss <a.derybowski@wp.pl>
2022-03-05 14:15:25 +0100

    ladana plansza

--- plansza.js
+++ plansza.js
@@ -1,12 +1,45 @@
 function tworzeniePlanszy() {
     let a = 1;
+    let b = 1;
+
     for(let i = 0; i < 9 ; i++)
     {
         for(let l = 0; l < 9; l++)
         {
-            document.getElementById("pole").innerHTML += "<div class = "+"plansza"+" id= " + a + " ><a class = " + "liczby" + ">" + a + "</a></div>";
+            document.getElementById("pole").innerHTML += "<div class = "+"plansza"+" id= " + a + " ><a class = " + "liczby" + ">" + a + "</a></div>";                        
             a++;
+        }      
+        
+    } 
+
+       
+    for(let j = 1 ; j <= 9; j++){
+        for(let i = 1; i <= 9; i++){
+            document.getElementById(j).style.backgroundColor = "rgb(77, 33, 33)"; 
+            document.getElementById(j+9).style.backgroundColor = "rgb(77, 33, 33)"; 
+            document.getElementById(j+18).style.backgroundColor = "rgb(77, 33, 33)";               
+        } 
+        if(j == 3){
+            j = 6;
         }
-    }
-    
+    }    
+
+    for(let j = 31 ; j <= 33; j++){
+        for(let i = 1; i <= 9; i++){
+            document.getElementById(j).style.backgroundColor = "rgb(77, 33, 33)"; 
+            document.getElementById(j+9).style.backgroundColor = "rgb(77, 33, 33)"; 
+            document.getElementById(j+18).style.backgroundColor = "rgb(77, 33, 33)"; 
+        }      
+    }    
+
+    for(let j = 55 ; j <= 63; j++){
+        for(let i = 1; i <= 9; i++){
+            document.getElementById(j).style.backgroundColor = "rgb(77, 33, 33)"; 
+            document.getElementById(j+9).style.backgroundColor = "rgb(77, 33, 33)"; 
+            document.getElementById(j+18).style.backgroundColor = "rgb(77, 33, 33)";          
+        } 
+        if(j == 57){
+            j = 60;
+        }
+    }    
 }
